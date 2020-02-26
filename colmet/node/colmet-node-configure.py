c36d0b31f3bc76f5f3539c0c85ee30daa739b538
Olivier Richard <olivier.richard@imag.fr>
2020-02-26 10:00:28 +0100

    [colmet-node] add main

--- colmet/node/colmet-node-configure.py
+++ colmet/node/colmet-node-configure.py
@@ -4,16 +4,20 @@ import msgpack
 from time import sleep
 import sys
 
-context = zmq.Context()
-sender = context.socket(zmq.PUSH)
+def main():
+    context = zmq.Context()
+    sender = context.socket(zmq.PUSH)
 
-sender.setsockopt(zmq.LINGER, 0)
-sender.setsockopt(zmq.SNDHWM, 1000)
+    sender.setsockopt(zmq.LINGER, 0)
+    sender.setsockopt(zmq.SNDHWM, 1000)
 
-sender.connect("tcp://127.0.0.1:5557")
+    sender.connect("tcp://127.0.0.1:5557")
 
-config = {"perfhw_metrics": sys.argv[2], "sample_period": sys.argv[1]}
+    config = {"perfhw_metrics": sys.argv[2], "sample_period": sys.argv[1]}
 
-print("config", config)
+    print("config", config)
 
-sender.send(msgpack.packb(config))
+    sender.send(msgpack.packb(config))
+
+if __name__ == '__main__':
+    main()
