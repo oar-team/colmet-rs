6985030aa23ca5c1f14bcc2bd118e436e879556d
Ilya Meignan Masson <imeignanmasson@fgrenoble>
2022-04-01 17:51:20 +0200

    modified script

--- setup_script.sh
+++ setup_script.sh
@@ -4,4 +4,4 @@ sudo-g5k ln -s /sys/fs/cgroup/perf_event /dev/oar_cgroups_links/
 sudo-g5k mkdir -p /dev/oar_cgroups_links/perf_event/$OAR_CPUSET
 echo $$ | sudo-g5k tee -a /dev/oar_cgroups_links/perf_event$OAR_CPUSET/tasks
 
-sudo-g5k apt install libzmq3-dev
+sudo-g5k apt install libzmq3-dev docker-compose
